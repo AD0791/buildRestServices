6d964970ad5e0644aefdf3b28171684b47d9297a
Alexandro Disla <alexandrodisla@hotmail.com>
2020-08-20 13:33:29 -0400

    make it simple

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const app = express();
 
+const data = require("./mock.json");
 
-
-
-app.get('/', (req, res) => {
-    res.send("let's get busy");
+app.get("/", (req, res) => {
+  res.json(data);
 });
-
-
-
-
 
 // env variables
 // terminal: $export PORT=5000
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`Port is open ${port}`);
-})
+  console.log(`Port is open ${port}`);
+});
